8412eb3e00d19f5001aa5af29a77d942430571bc
celt1125 <andysh1125@gmail.com>
2022-03-18 01:26:47 +0800

    add description

--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
 python run.py -fp CAJUN-FOLK-SONGS-II.pdf -if png -pp C:\\D_Drive\\Software\\poppler-22.01.0\\Library\\bin
+
+:: -fp: input pdf file name
+:: -if: image format. 'jpg' will be faster. 'png' will be slower but better
+:: -pp: path to the bin directory of poppler
